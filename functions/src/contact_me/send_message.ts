d71f7d62020b1e5b3fe0d4a9cec89f268ee976f5
William Kondo <willkondo@gmail.com>
2021-04-02 15:34:38 -0300

    Validation in send message cloud function

--- functions/src/contact_me/send_message.ts
+++ functions/src/contact_me/send_message.ts
@@ -1,6 +1,35 @@
 import * as functions from 'firebase-functions';
 
-export const call = functions.https.onCall((data, context) => {
-  console.log(data);
-  console.log(context);
+type Form = {
+  name: string;
+  contact: string;
+  message: string;
+};
+
+const SINGLE_LINE_MAX_LENGTH = 256;
+const MULTI_LINE_MAX_LENGTH = 65536;
+
+const isFormValid = (form: Form) => {
+  const { name = '', contact = '', message = '' } = form;
+
+  if (!name.trim() || name.trim().length > SINGLE_LINE_MAX_LENGTH) return false;
+  if (!contact.trim() || contact.trim().length > SINGLE_LINE_MAX_LENGTH)
+    return false;
+  if (!message.trim() || message.trim().length > MULTI_LINE_MAX_LENGTH)
+    return false;
+
+  return true;
+};
+
+export const call = functions.https.onCall((data) => {
+  const { form } = data;
+
+  if (!form)
+    throw new functions.https.HttpsError(
+      'invalid-argument',
+      'form cannot be empty'
+    );
+
+  if (!isFormValid(form))
+    throw new functions.https.HttpsError('invalid-argument', 'form is invalid');
 });
